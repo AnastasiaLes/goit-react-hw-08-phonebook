96228177ec08a21604190893c62fd077c7c96f0d
Anastasia Les <anastasia@sonarmedia.com>
2022-06-01 17:25:13 +0300

    fixing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,19 +7,19 @@ import AppBar from 'components/appBar/AppBar';
 import { fetchCurrentUser } from 'redux/auth/authOperations';
 
 const HomeView = lazy(() =>
-  import('../../views/HomeView' /* webpackChunkName: "home-page" */)
+  import('../views/HomeView' /* webpackChunkName: "home-page" */)
 );
 const Welcome = lazy(() =>
-  import('../../views/WelcomeView' /* webpackChunkName: "welcome-page" */)
+  import('../views/WelcomeView' /* webpackChunkName: "welcome-page" */)
 );
 const LoginView = lazy(() =>
-  import('../../views/LoginView' /* webpackChunkName: "login-page" */)
+  import('../views/LoginView' /* webpackChunkName: "login-page" */)
 );
 const RegisterView = lazy(() =>
-  import('../../views/RegisterView' /* webpackChunkName: "register-page" */)
+  import('../views/RegisterView' /* webpackChunkName: "register-page" */)
 );
 const ContactsView = lazy(() =>
-  import('../../views/ContactView' /* webpackChunkName: "contacts-page" */)
+  import('../views/ContactView' /* webpackChunkName: "contacts-page" */)
 );
 
 export function App() {
